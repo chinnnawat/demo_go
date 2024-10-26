b1e40279851263f58430b8c2caf3bce54aba3d01
unknown <chin3896@gmail.com>
2024-10-26 19:59:50 +0700

    on hold

--- backend/core/main.go
+++ backend/core/main.go
@@ -18,7 +18,6 @@ var albums = []Album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan", Artist: "Sarah Vaughan", Price: 39.99},
-	{ID: "4", Title: "Sarah Vaughan", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
 func getAlbums(c *gin.Context) {
